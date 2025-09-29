c8fdd87b9b749a62a42a4e3a0310a602b5f6ec57
tihada <c2tihada@vasvari.org>
2025-09-29 09:30:50 +0200

    fájlok, feladat2

---
+++ feladat2.py
@@ -0,0 +1,5 @@
+def Teglalap():
+    a = float(input("Téglalap egyik oldala: "))
+    b = float(input("Téglalap másik oldala: "))
+
+    print(f"Téglalap területe: {a*b}\nA téglalap kerülete {2*(a+b)}")
